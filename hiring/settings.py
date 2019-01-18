42db89592e06b5d3981f0b4d108ceef1c9926f74
Alan Soder <alanbr4@gmail.com>
2019-01-18 15:30:46 -0300

    Finished the signup system

--- hiring/settings.py
+++ hiring/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'OPTIONS': {
-            'read_default_file': os.path.join(BASE_DIR, 'hiring/mysql.cnf'),
+            'read_default_file': os.path.join(BASE_DIR, 'mysql.cnf'),
         },
     }
 }
